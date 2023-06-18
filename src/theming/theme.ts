56eb7094247939b34c0dadf54ae884893709b3a0
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-06-17 21:43:58 -0700

    adds theming functionality

--- src/theming/theme.ts
+++ src/theming/theme.ts
@@ -1,14 +1,58 @@
-// theme.ts
-export const lightTheme = {
-    body: '#FFF',
-    text: '#363537',
-    toggleBorder: '#FFF',
-    gradient: 'linear-gradient(#39598A, #79D7ED)',
+export interface Theme {
+    body: string;
+    text: string;
+    toggleBorder: string;
+    gradient: string;
+    cardBackground: string;
+    primary: string;
+    secondary: string;
+    accent: string;
+    hover: string;
+    shadow: string;
+    borderRadius: string;
+    transition: string;
+    fontFamily: string;
+    error: string;
+    success: string;
+    cardColor: string;
 }
 
-export const darkTheme = {
-    body: '#363537',
-    text: '#F0F0F0',
+// lightTheme.ts
+export const lightTheme: Theme = {
+    body: 'rgba(220,220,220,1)',
+    text: 'rgba(0,0,0,1)',
+    toggleBorder: '#FFF',
+    gradient: 'linear-gradient(#39598A, #79D7ED)',
+    cardBackground: 'rgba(250,250,250,0.8)',
+    primary: 'rgba(255, 0, 200, 1)',
+    secondary: 'rgba(0, 255, 200, 1)',
+    accent: 'rgba(200, 0, 255, 1)',
+    hover: 'rgba(0, 0, 255, 0.5)',
+    shadow: '0px 0px 20px rgba(0, 0, 0, 0.2)',
+    borderRadius: '10px',
+    transition: 'all 0.3s linear',
+    fontFamily: '"Roboto", sans-serif',
+    error: 'rgba(255, 0, 0, 1)',
+    success: 'rgba(0, 255, 0, 1)',
+    cardColor: 'rgba(250,250,250,0.9)'
+};
+
+// darkTheme.ts
+export const darkTheme: Theme = {
+    body: 'rgba(10,10,10,1)',
+    text: 'rgba(255,255,255,1)',
     toggleBorder: '#6B8096',
     gradient: 'linear-gradient(#091236, #1E215D)',
-}
+    cardBackground: 'rgba(5,5,5,0.8)',
+    primary: 'rgba(0, 255, 0, 1)',
+    secondary: 'rgba(255, 0, 0, 1)',
+    accent: 'rgba(0, 0, 255, 1)',
+    hover: 'rgba(255, 255, 255, 0.5)',
+    shadow: '0px 0px 20px rgba(255, 255, 255, 0.2)',
+    borderRadius: '10px',
+    transition: 'all 0.3s linear',
+    fontFamily: '"Roboto", sans-serif',
+    error: 'rgba(255, 0, 0, 1)',
+    success: 'rgba(0, 255, 0, 1)',
+    cardColor: 'rgba(5,5,5,0.9)'
+};
