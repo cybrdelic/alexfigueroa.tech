87d32c036e8172a751bf940669e64487a72049f8
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-06-17 21:44:24 -0700

    adds mock data

---
+++ src/data/project.data.tsx
@@ -0,0 +1,27 @@
+export type ProjectType = {
+    id: string;
+    name: string;
+}
+
+export const projectsData: ProjectType[] = [
+    {
+        id: '12367567',
+        name: 'CodebaseCraft.ai'
+    },
+    {
+        id: '45857389754',
+        name: "Ech0Mind.ai"
+    },
+    {
+        id: '43789498725',
+        name: 'Cybrnet'
+    },
+    {
+        id: '437837578493',
+        name: 'NexusDb'
+    },
+    {
+        id: '3485675',
+        name: 'alexfigueroa.tech'
+    }
+]
