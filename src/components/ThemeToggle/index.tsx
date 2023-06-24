a67fdc7d54041ab8fac5697f87103d1f4037f5ae
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-06-24 00:07:23 -0700

    create themeToggle component

--- src/components/ThemeToggle/index.tsx
+++ src/components/ThemeToggle/index.tsx
@@ -1,24 +1,33 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 import IconButton from '@mui/material/IconButton';
 import Brightness3Icon from '@mui/icons-material/Brightness3'; // Moon icon
 import WbSunnyIcon from '@mui/icons-material/WbSunny'; // Sun icon
 import { useTheme, useToggleTheme } from '../../hooks/useTheme';
 import { lightTheme } from '../../theming/theme';
 
+const StyledIconButton = styled(IconButton)`
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    
+`;
+
 const ThemeToggle = ({onClick}: {onClick: () => void}) => {
     const theme = useTheme();
     
     return (
-      <IconButton
-        edge="end"
-        color="inherit"
-        aria-label="mode"
-        onClick={onClick}
-      >
-        {theme === lightTheme ? <Brightness3Icon /> : <WbSunnyIcon />}
-      </IconButton>
+        <StyledIconButton
+            edge="end"
+            color="inherit"
+            aria-label="mode"
+            onClick={onClick}
+        >
+            {theme === lightTheme 
+              ? <Brightness3Icon fontSize="large" /> 
+              : <WbSunnyIcon fontSize="large" />}
+        </StyledIconButton>
     );
-  };
-  
-  export default ThemeToggle;
-  
+};
+
+export default ThemeToggle;
