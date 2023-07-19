96a3c45761ce4aa323b6f79529a61e21e1344dd7
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-07-18 18:41:43 -0700

    fixes styling in ProjectPreview to fix layout, spacing, and colors

--- src/components/ProjectsView/ProjectPreview/index.tsx
+++ src/components/ProjectsView/ProjectPreview/index.tsx
@@ -1,34 +1,40 @@
 import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { useTheme } from "../../../hooks/useTheme";
 import { ProjectType } from "../../../data/project.data";
-import { createStyledMotionComponent } from "../../../utils/createStyledMotionComponent";
 import { ProjectTitleAndLogo } from "../ProjectLogoAndText";
+import { createStyledMotionComponent } from "../../../utils/createStyledMotionComponent";
 
 const ProjectPreviewWrapper = createStyledMotionComponent('div')(props => `
     display: flex;
-    flex-direction: column; // Changed from row to column to give more space
-    justify-content: center; // Adjust as needed
-    align-items: center; // Adjust as needed
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     height: 40rem;
-    margin-left: 2rem;
+    margin: 2rem;
     border-radius: 15px;
-    min-width: 73vw;
-    max-width: 73vw;
-    margin-right: 2rem;
+    width: 73vw;
     background: ${props.theme.gradient};
     box-shadow: 0px 10px 30px -5px rgba(0, 0, 0, 0.5);
-    padding: 2rem; // Add padding inside the wrapper
+    padding: 2rem;
+
+    @media (max-width: 768px) {
+        width: 90vw;
+    }
 `);
 
 const StyledProjectOverview = createStyledMotionComponent('p')(props => `
     color: ${props.theme.text};
-    margin-bottom: 2rem; // Added margin-bottom for more space between elements
+    margin-bottom: 2rem;
 `);
 
 const StyledProjectDetailsLink = createStyledMotionComponent('div')(props => `
-    text-align: center;
+    display: flex;
+    justify-content: flex-end;
+    grid-gap: 2rem;
     padding-top: 1rem;
-    margin: 1rem; // add margin to give more spacing between elements
+    margin: 1rem;
+    width: 100%;
     a {
         color: ${props.theme.text};
         text-decoration: none;
@@ -36,27 +42,48 @@ const StyledProjectDetailsLink = createStyledMotionComponent('div')(props => `
         border: 1px solid ${props.theme.text};
         border-radius: 5px;
         transition: background-color 0.3s ease-in-out, color 0.3s ease-in-out;
+
         &:hover {
             background-color: ${props.theme.text};
             color: ${props.theme.background};
         }
+
+        @media (max-width: 768px) {
+            margin-bottom: 0.5rem;
+        }
+    }
+
+    @media (max-width: 768px) {
+        flex-direction: column;
     }
 `);
 
+const fadeIn = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 0.5 } },
+};
+
 export const ProjectPreview = ({ project }: { project: ProjectType | null }) => {
-    const theme = useTheme()
-    if (!project) return null;
+    const theme = useTheme();
+
+    if (!project) return <div>No project selected.</div>;
 
     return (
-        <ProjectPreviewWrapper theme={theme}>
-            <ProjectTitleAndLogo project={project} />
-            <StyledProjectOverview theme={theme}>{project.overview}</StyledProjectOverview>
-            <StyledProjectDetailsLink theme={theme}>
-                <a href={`/project/${project.name}`} target="_blank" rel="noopener noreferrer">View Details</a>
-            </StyledProjectDetailsLink>
-            <StyledProjectDetailsLink theme={theme}>
-                <a href={project.github_url} target="_blank" rel="noopener noreferrer">View Repo</a>
-            </StyledProjectDetailsLink>
-        </ProjectPreviewWrapper>
+        <AnimatePresence mode="wait">
+            <ProjectPreviewWrapper initial="hidden" animate="show" variants={fadeIn} theme={theme}>
+                <ProjectTitleAndLogo project={project} />
+                <StyledProjectOverview theme={theme}>{project.overview}</StyledProjectOverview>
+                <StyledProjectDetailsLink theme={theme}>
+                    <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+                        <a href={`/project/${project.name}`} target="_blank" rel="noopener noreferrer">View Details</a>
+                    </motion.div>
+                    <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }} style={{
+                        backgroundColor: project.primaryColor
+                    }}>
+                        <a href={project.github_url} target="_blank" rel="noopener noreferrer">View Repo</a>
+                    </motion.div>
+                </StyledProjectDetailsLink>
+            </ProjectPreviewWrapper>
+        </AnimatePresence>
     );
-}
+};
