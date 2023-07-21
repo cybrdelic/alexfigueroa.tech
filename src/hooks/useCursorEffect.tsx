0b1f09f5bb0459573e464c032667605914151dde
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-07-20 18:15:15 -0700

    adds click effect to cursor

--- src/hooks/useCursorEffect.tsx
+++ src/hooks/useCursorEffect.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useContext } from 'react';
 
 export const useCursorEffect = () => {
-  const [cursorType, setCursorType] = useState<'normal' | 'hovered'>('normal');
+  const [cursorType, setCursorType] = useState<'normal' | 'hovered' | 'clicked'>('normal');
 
   useEffect(() => {
     const elements = document.querySelectorAll('[data-id="special"], a, li, button');
@@ -14,15 +14,29 @@ export const useCursorEffect = () => {
       setCursorType('normal');
     };
 
+    const mousedownFunc = () => {
+      setCursorType('clicked');
+    };
+
+    const mouseupFunc = () => {
+      // Reset to 'normal' or 'hovered' based on if the mouse is still over the element.
+      // This assumes that your elements cannot be simultaneously hovered and clicked.
+      setCursorType(document.querySelectorAll(':hover').length > 0 ? 'hovered' : 'normal');
+    };
+
     elements.forEach(element => {
       element.addEventListener('mouseover', mouseoverFunc);
       element.addEventListener('mouseout', mouseoutFunc);
+      element.addEventListener('mousedown', mousedownFunc);
+      element.addEventListener('mouseup', mouseupFunc);
     });
 
     return () => {
       elements.forEach(element => {
         element.removeEventListener('mouseover', mouseoverFunc);
         element.removeEventListener('mouseout', mouseoutFunc);
+        element.removeEventListener('mousedown', mousedownFunc);
+        element.removeEventListener('mouseup', mouseupFunc);
       });
     };
   }, []);
