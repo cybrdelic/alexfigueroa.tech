5f7eab6cd6a981595469d41d3ecfbe7dfcecb21e
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-08-27 10:00:23 -0700

    implements some style-guide tools in ProjectPicker component

--- src/components/ProjectsView/ProjectPicker/index.tsx
+++ src/components/ProjectsView/ProjectPicker/index.tsx
@@ -3,32 +3,45 @@ import { ProjectData, ProjectType } from "../../../data/project.data";
 import { createStyledMotionComponent } from "../../../utils/createStyledMotionComponent";
 import { GridElement } from "../GridElement";
 import { adjustTransparency } from "../../../utils/adjustTransparency";
+import { useCursorEffect } from "../../../hooks/useCursorEffect";
+import { spacing } from "../../../theming/style-guide/spacing";
+import { zIndex } from "../../../theming/style-guide/zIndex";
 
 interface ProjectPickerProps {
     projects: ProjectData,
     selectedProject: ProjectType | null,
     setSelectedProject: (project: ProjectType | null) => void,
 }
+interface ProjectPickerProps {
+    projects: ProjectData,
+    selectedProject: ProjectType | null,
+    setSelectedProject: (project: ProjectType | null) => void,
+}
 
-const ProjectPickerWrapper = createStyledMotionComponent('div')(props => `
+const ProjectPickerWrapper: any = createStyledMotionComponent('div')(props => `
     display: flex;
-    flex-direction: row;
     justify-content: center;
-    align-items: start;
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-    box-sizing: border-box;
-    border-radius: 60px;
-    margin: 30rem;
-    background-color: ${adjustTransparency(props.theme.cardBackground, 0.9)};
-    z-index: 9999;
+    flex-direction: column;
 `);
 
+const ProjectPickerContainer = createStyledMotionComponent('div')(props => `
+    display: flex;
+    z-index: ${zIndex.dropdown};
+    background-color: ${adjustTransparency(props.theme.cardBackground, 0.9)};
+    justify-content: space-evenly;
+    width: 100%;
+    bottom: ${spacing.sm};
+    @media (min-width: 768px) {
+      bottom: ${spacing.md};
+    }
+`)
 
 const GridElementContainer = createStyledMotionComponent('div')(props => `
-    display: flex;
-    padding: 2rem;
-
+  display: flex;
+  padding: ${spacing.sm};
+  @media (min-width: 768px) {
+    padding: ${spacing.md};
+  }
 `)
 
 const MemoizedGridElement = memo(GridElement);
@@ -55,7 +68,8 @@ const gridTransition = {
 const listVariants = {
     initial: {
         scale: 0,
-        y: '200px'
+        y: '200px',
+        x: 0
     },
     in: {
         transition: {
@@ -63,12 +77,17 @@ const listVariants = {
             delayChildren: 0.3 // this will delay the animation of all children by 0.3s
         },
         y: 0,
-        scale: 1
+        scale: 1,
+        x: 0
+    },
+    exit: {
+        x: -100
     }
 };
 
 export default function ProjectPicker(props: ProjectPickerProps) {
     const { projects, selectedProject, setSelectedProject } = props;
+    const cursorType = useCursorEffect();
 
     const handleMouseEnter = (project: ProjectType) => {
         setSelectedProject(project);
@@ -82,21 +101,23 @@ export default function ProjectPicker(props: ProjectPickerProps) {
 
     return (
         <ProjectPickerWrapper variants={listVariants}>
-            {Object.values(projects).map((project, index) => (
-                <GridElementContainer
-                    key={project.id}
-                    transition={gridTransition}
-                    custom={index}
-                    variants={gridElementVariants}
-                >
-                    <MemoizedGridElement
-                        project={project}
-                        handleMouseEnter={handleMouseEnter}
-                        isActive={selectedProject?.id === project.id}
-                        handleMouseLeave={handleMouseLeave}
-                    />
-                </GridElementContainer>
-            ))}
+            <ProjectPickerContainer>
+                {Object.values(projects).map((project, index) => (
+                    <GridElementContainer
+                        key={project.id}
+                        transition={gridTransition}
+                        custom={index}
+                        variants={gridElementVariants}
+                    >
+                        <MemoizedGridElement
+                            project={project}
+                            handleMouseEnter={handleMouseEnter}
+                            isActive={selectedProject?.id === project.id}
+                            handleMouseLeave={handleMouseLeave}
+                        />
+                    </GridElementContainer>
+                ))}
+            </ProjectPickerContainer>
         </ProjectPickerWrapper>
     )
 }
