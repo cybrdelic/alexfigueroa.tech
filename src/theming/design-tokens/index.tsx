59bbcc7ed09b69361601c44dd1c005727439336e
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-09-01 23:09:44 -0700

    refactors theme and colors to work with new design tokens

---
+++ src/theming/design-tokens/index.tsx
@@ -0,0 +1,5 @@
+export * from "./colors";
+export * from "./effects";
+export * from "./responsive";
+export * from "./spacing";
+export * from "./typography";
--- src/theming/theme.ts
+++ src/theming/theme.ts
@@ -1,3 +1,5 @@
+import * as tokens from "./design-tokens";
+
 export interface Theme {
     body: string;
     text: string;
@@ -18,44 +20,32 @@ export interface Theme {
     name: string;
 }
 
-// lightTheme.ts
-export const lightTheme: Theme = {
-    body: 'rgba(220,220,220,1)',
-    text: 'rgba(0,0,0,1)',
-    toggleBorder: '#FFF',
-    gradient: 'linear-gradient(rgba(250,250,250,1), rgba(240,240,240,1))',
-    cardBackground: 'rgba(240,240,240,0.8)',
-    primary: 'rgba(255, 0, 200, 1)',
-    secondary: 'rgba(0, 255, 200, 1)',
-    accent: 'rgba(200, 0, 255, 1)',
-    hover: 'rgba(0, 0, 255, 0.5)',
-    shadow: '0px 0px 20px rgba(0, 0, 0, 0.2)',
-    borderRadius: '10px',
-    transition: 'all 0.3s linear',
-    fontFamily: '"Roboto", sans-serif',
-    error: 'rgba(255, 0, 0, 1)',
-    success: 'rgba(0, 255, 0, 1)',
-    cardColor: 'rgba(250,250,250,0.9)',
-    name: 'lightTheme'
+const common = {
+    typography: tokens.typography,
+    transitions: tokens.transitions,
+    spacing: tokens.spacing,
+    zIndex: tokens.zIndex,
+    breakpoints: tokens.breakpoints,
+    borderRadius: tokens.borderRadius,
+    boxShadow: tokens.boxShadow,
 };
 
-// darkTheme.ts
-export const darkTheme: Theme = {
-    body: 'rgba(10,10,10,1)',
-    text: 'rgba(255,255,255,1)',
-    toggleBorder: '#6B8096',
-    gradient: 'linear-gradient(rgba(0,0,0,1), rgba(10,10,10,1))',
-    cardBackground: 'rgba(5,5,5,0.8)',
-    primary: 'rgba(0, 255, 0, 1)',
-    secondary: 'rgba(255, 0, 0, 1)',
-    accent: 'rgba(0, 0, 255, 1)',
-    hover: 'rgba(255, 255, 255, 0.5)',
-    shadow: '0px 0px 20px rgba(255, 255, 255, 0.2)',
-    borderRadius: '10px',
-    transition: 'all 0.3s linear',
-    fontFamily: '"Roboto", sans-serif',
-    error: 'rgba(255, 0, 0, 1)',
-    success: 'rgba(0, 255, 0, 1)',
-    cardColor: 'rgba(5,5,5,0.9)',
-    name: 'darkTheme'
-};
+const createTheme = (colorMode: 'dark' | 'light') => ({
+    ...common,
+    colors: {
+        primary: tokens.colors.primary[colorMode],
+        secondary: tokens.colors.secondary[colorMode],
+        success: tokens.colors.success[colorMode],
+        danger: tokens.colors.danger[colorMode],
+        warning: tokens.colors.warning[colorMode],
+        info: tokens.colors.info[colorMode],
+        light: tokens.colors.light[colorMode],
+        dark: tokens.colors.dark[colorMode],
+        gray: tokens.colors.gray[colorMode],
+        text: tokens.colors.common[colorMode === 'dark' ? 'white' : 'black'],
+        background: tokens.colors.dark[colorMode],
+    }
+});
+
+export const lightTheme = createTheme('dark');
+export const darkTheme = createTheme('light');
