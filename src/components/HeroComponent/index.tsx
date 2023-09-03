5ee5e91d8f9ad22fdb47db4fe2512f32e96d470a
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-09-02 20:36:15 -0700

    implements new theming system with HeroComponent

--- src/components/HeroComponent/index.tsx
+++ src/components/HeroComponent/index.tsx
@@ -14,6 +14,7 @@ import { padding } from '../../theming/util-style-functions/spacing';
 import { backgroundColor, textColor } from '../../theming/util-style-functions/colors';
 import { createStyledMotionComponent } from '../../theming/styled-motion-utils/createStyledMotionComponent';
 import { rounded } from '../../theming/util-style-functions/misc';
+import { ThemeProvider } from '../../theming/ThemeProvider';
 
 const HeroContainer = styled(motion.div)`
   ${flexColumn}
@@ -36,8 +37,8 @@ const RightPane = styled(Pane)`
 
 const HeroTitle = createStyledMotionComponent(animated.h1)(props => `
   font-size:5rem;
-  ${textColor('text', props?.theme?.mode)};
-  text-shadow: 0px 0px 10px ${props?.theme?.colors?.primary};
+  ${textColor(props.theme, 'text')}
+  text-shadow: 0px 0px 10px ${props?.theme?.colors?.text};
   text-align: flex-start;
   font-family: 'Orbitron';
   width: 100%;
@@ -45,7 +46,7 @@ const HeroTitle = createStyledMotionComponent(animated.h1)(props => `
 
 const HeroTagline = createStyledMotionComponent(animated.h2)(props => `
   font-size: 1.5rem;
-  ${textColor('secondary', props?.theme?.mode)}
+  ${textColor(props.theme, 'text')}
   text-shadow: 0px 0px 10px ${props?.theme?.colors?.secondary};
   text-aligh: flex-start;
   width: 100%;
@@ -54,16 +55,15 @@ const HeroTagline = createStyledMotionComponent(animated.h2)(props => `
 `);
 
 const HeroButton = createStyledMotionComponent('button')(props => `
-  ${backgroundColor('background', props?.theme?.mode)}
-  ${textColor('secondary')}
+  ${backgroundColor(props.theme, 'background')}
+  ${textColor(props.theme, 'text')};
   ${padding('md')}
   border: none;
   ${rounded('lg')}
   cursor: pointer;
-  transition: transform 0.3s ease-in-out;
+  transition: transform 0.3s ease -in -out;
   text-transform: uppercase;
   font-weight: 900;
-
 
   &:hover {
     transform: scale(1.1);
@@ -86,21 +86,6 @@ const ButtonContainer = styled(motion.div)`
   margin-top: 4rem;
 `
 
-
-const containerVariants = {
-  hidden: { opacity: 0 },
-  visible: { opacity: 1, transition: { delay: 0.5, duration: 1.5 } },
-  exit: { opacity: 0, transition: { ease: 'easeInOut' } },
-};
-
-
-const TEXTS = [
-  "Full Stack, Full Circle - From Idea Generation to Implementation to Deployment",
-  "Where Innovation Intersects with Implementation: Crafting Digital Experiences",
-  "Reshaping the Digital Landscape: Expertise that Delivers Impact",
-  "Beyond Code: Crafting Interconnected Systems for a Smarter World"
-];
-
 const HeroComponent = () => {
   const theme = useTheme();
   const [index, setIndex] = React.useState(0);
@@ -115,25 +100,27 @@ const HeroComponent = () => {
   });
 
   return (
-    <HeroContainer>
-      <RightPane>
-        <HeroTitle theme={theme} style={springProps}>
-          <div data-id="special">
-            <div>Implementing solutions</div>
-          </div>
-        </HeroTitle>
+    <ThemeProvider>
+      <HeroContainer>
+        <RightPane>
+          <HeroTitle theme={theme} style={springProps}>
+            <div data-id="special">
+              <div>Implementing solutions</div>
+            </div>
+          </HeroTitle>
 
-        <HeroTagline theme={theme} style={springProps}>
-          Full-Stack Software Engineer
-        </HeroTagline>
-        <ButtonContainer>
-          <HeroButton>
-            View Projects
-          </HeroButton>
-        </ButtonContainer>
+          <HeroTagline theme={theme} style={springProps}>
+            Full-Stack Software Engineer
+          </HeroTagline>
+          <ButtonContainer>
+            <HeroButton theme={theme}>
+              View Projects
+            </HeroButton>
+          </ButtonContainer>
 
-      </RightPane>
-    </HeroContainer>
+        </RightPane>
+      </HeroContainer>
+    </ThemeProvider>
   );
 };
 export default HeroComponent
