b81f048a5abfc8bce41583a76304877129b2e823
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-09-07 19:03:33 -0700

    fixes carousel component to work with framer motion

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -4,25 +4,28 @@ import { createStyledMotionComponent } from '../../theming/styled-motion-utils/c
 
 import styled, { css, keyframes } from 'styled-components';
 
-const blurIn = keyframes`
-  from {
-    filter: blur(5px);
-    transform: scale(0.8);
-  }
-  to {
-    filter: none;
-    transform: scale(1);
-  }
+const pulsate = keyframes`
+  0% { transform: scale(1); }
+  50% { transform: scale(1.05); }
+  100% { transform: scale(1); }
 `;
 
-const blurOut = keyframes`
-  from {
-    filter: none;
-    transform: scale(1);
-  }
-  to {
-    filter: blur(5px);
-    transform: scale(0.8);
+const NextButton = styled.button`
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 8px 16px;
+  border: none;
+  border-radius: 8px;
+  background-color: #333;
+  color: white;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  animation: ${pulsate} 1.5s infinite;  // Pulsating animation
+
+  &:hover {
+    background-color: #555;
   }
 `;
 
@@ -34,38 +37,46 @@ export const Container = createStyledMotionComponent('div')(props => `
   perspective: 1500px;
 `);
 
-export const CarouselItem = createStyledMotionComponent('div')(props => `
+export const CarouselItem = createStyledMotionComponent('div')(props => css`
   position: absolute;
   top: 10%;
-  transform: translateY(-50%);
   width: 80%;
   height: 80%;
-  background: ${props.bgColor};
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: 2rem;
   border-radius: 10px;
-
-  /* Position and styles for the active item */
-  ${props.active && css`
-    left: 10%;
-    top: 10%;
-    filter: none;
-    transform: translateY(-50%) scale(1);
-    z-index: 2;
-  `}
-
-  /* Position and styles for the inactive item */
-  ${!props.active && css`
-    left: 60%;
-    top: 10%;
-    filter: blur(5px);
-    transform: translateY(-50%) scale(0.8);
-    z-index: 1;
-  `}
+  transition: all 0.5s ease-in-out;
+  transform-style: preserve-3d;
+  filter: brightness(1.2);  // Slightly increased brightness for a glowing effect
 `);
 
+const getAnimationProps = (status) => {
+    switch (status) {
+        case 'active':
+            return {
+                transform: "rotateY(0deg) scale(1.05)",  // Slightly larger scale
+                left: 0,
+                filter: 'blur(0px) brightness(1.2)'
+            };
+        case 'previous':
+            return {
+                transform: "rotateY(-45deg) scale(0.5)",  // Added depth with translateZ
+                left: "-300%",
+                filter: 'blur(60px) brightness(0.5)'
+            };
+        case 'next':
+            return {
+                transform: "rotateY(45deg) scale(0.8)",  // Added depth with translateZ
+                zIndex: 0,
+                left: '50%',
+                filter: 'blur(40px) brightness(0.5)'
+            };
+        default:
+            return {};
+    }
+};
 
 const Carousel = ({ items }) => {
     const [activeIndex, setActiveIndex] = useState(0);
@@ -74,26 +85,46 @@ const Carousel = ({ items }) => {
         setActiveIndex((prev) => (prev + 1) % items.length);
     };
 
+    const getPreviousIndex = () => (activeIndex - 1 + items.length) % items.length;
+    const getNextIndex = () => (activeIndex + 1) % items.length;
+
     return (
         <Container>
             <LayoutGroup>
-                {items.map((item, index) => (
-                    <CarouselItem
-                        layout // Enables the layout animation
-                        layoutId={item.color}
-                        key={item.color} // Using color for key just for the demo. Consider a unique ID in a real-world scenario.
-                        active={index === activeIndex}
-                        bgColor={item.color}
-                    >
-                        <div>
-                            {item.content}
-                        </div>
-                    </CarouselItem>
-                ))}
+                <CarouselItem
+                    layout
+                    layoutId={`item-${getPreviousIndex()}`} // Using index as layoutId
+                    key={getPreviousIndex()}  // Using index as key
+                    initial={false}
+                    animate={getAnimationProps("previous")}
+                >
+                    {items[getPreviousIndex()]}
+                </CarouselItem>
+
+                <CarouselItem
+                    layout
+                    layoutId={`item-${activeIndex}`}  // Using index as layoutId
+                    key={activeIndex}  // Using index as key
+                    initial={false}
+                    animate={getAnimationProps("active")}
+                >
+                    {items[activeIndex]}
+                </CarouselItem>
+
+                <CarouselItem
+                    layout
+                    layoutId={`item-${getNextIndex()}`}  // Using index as layoutId
+                    key={getNextIndex()}  // Using index as key
+                    initial={false}
+                    animate={getAnimationProps("next")}
+                >
+                    {items[getNextIndex()]}
+                </CarouselItem>
             </LayoutGroup>
-            <button style={{ position: 'absolute', bottom: '10px', left: '50%', transform: 'translateX(-50%)' }} onClick={next}>Next</button>
+            <NextButton onClick={next}>Next</NextButton>
         </Container>
     );
 }
+
 
 export default Carousel;
