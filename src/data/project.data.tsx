e781c271bbccf452e562a6d52612155f6371387b
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-11-11 15:25:48 -0800

    re implements titleFonts

--- src/data/project.data.tsx
+++ src/data/project.data.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { ProjectBrandingType, getProjectBrandingCopy } from './utils/getProjectBrandingCopy';
 import { getProjectUrls } from './utils/getProjectUrls';
+import { getProjectTitleFonts } from './utils/getProjectTitleFonts';
 
 export type ProjectKey = 'WorkspaceAutomator' | 'Blitzkrieg' | 'DynamicTaskLine' | 'TraceMate' | 'CodebaseSeed' | 'CodebasedUtils' | 'AlexFigueroaPortfolio';
 
 export type ProjectType = {
     id: string;
     branding: ProjectBrandingType;
-    github_url: string; // Keep the GitHub URL here
+    github_url: string;
+    title_font: string;// Keep the GitHub URL here
     // Add any other fields you might need
 };
 
@@ -19,6 +21,7 @@ export type ProjectData = {
 const fetchProjectData = (): ProjectData => {
     const brandingCopy = getProjectBrandingCopy();
     const projectUrls = getProjectUrls();
+    const projectTitleFonts = getProjectTitleFonts();
 
     return Object.keys(brandingCopy).reduce((acc, key) => {
         const projectKey = key as ProjectKey;
@@ -26,6 +29,7 @@ const fetchProjectData = (): ProjectData => {
             id: uuidv4(),
             branding: brandingCopy[projectKey],
             github_url: projectUrls[projectKey],
+            title_font: projectTitleFonts[projectKey]
         };
         return acc;
     }, {} as ProjectData);
