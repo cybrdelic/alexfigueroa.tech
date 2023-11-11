73ffa30f6843a5d0eff9400ee48224f16cb954be
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-11-11 15:08:54 -0800

    implements new project title fonts

---
+++ src/data/utils/getProjectTitleFonts.tsx
@@ -0,0 +1,19 @@
+export const getProjectTitleFonts = () => {
+    const WorkspaceAutomator = 'Roboto Mono';
+    const Blitzkrieg = 'Major Mono Display';
+    const DynamicTaskLine = 'Audiowide';
+    const TraceMate = 'Share Tech Mono';
+    const CodebaseSeed = 'Press Start 2P';
+    const CodebasedUtils = 'Orbitron'; // Using Orbitron for one of your projects
+    const AlexFigueroaPortfolio = 'Roboto Mono'; // Reusing Roboto Mono for portfolio
+
+    return {
+        WorkspaceAutomator,
+        Blitzkrieg,
+        DynamicTaskLine,
+        TraceMate,
+        CodebaseSeed,
+        CodebasedUtils,
+        AlexFigueroaPortfolio
+    };
+};
