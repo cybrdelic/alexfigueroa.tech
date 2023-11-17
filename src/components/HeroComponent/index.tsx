3a6d0bcfef09c04ea6e7e5840300d97023937c7f
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-11-16 22:05:14 -0800

    adds New components to HeroComponent

--- src/components/HeroComponent/index.tsx
+++ src/components/HeroComponent/index.tsx
@@ -9,6 +9,10 @@ import { createStyledMotionComponent } from '../../theming/styled-motion-utils/c
 import { rounded } from '../../theming/util-style-functions/misc';
 import { fontFamily, fontSize, fontWeight, letterSpacing } from '../../theming/util-style-functions/typography';
 import { projectsData } from '../../data/project.data';
+import GitHubHeatmapContainer from '../GithubHeatmapContainer';
+import { FaDatabase, FaDocker, FaGitAlt, FaNodeJs, FaPython, FaReact } from 'react-icons/fa';
+import { adjustTransparency } from '../../utils/adjustTransparency';
+import { Theme } from '../../theming/theme';
 
 const HeroContainer = styled(motion.div)`
   display: flex;
@@ -57,7 +61,7 @@ const CTASection = styled(Pane)`
   flex-direction: row;
   justify-content: flex-end;
   align-items: flex-start;
-  width: 30%;
+  width: 100%;
 `;
 
 const RightPane = styled('div')`
@@ -178,14 +182,15 @@ const BlogPreview = createStyledMotionComponent('div')(props => css`
 `);
 
 const TechAndToolsSection = createStyledMotionComponent('div')(props => css`
-  ${flexColumn}
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
   ${fontSize('small')}
   ${fontFamily('monospace')}
   ${fontWeight('bold')}
   color: ${props => props.theme.colors.text};
   background-color: black;
-  width: 100%;
-  flex-grow: 1;
+  flex: ${props.flex}
 `);
 
 const HeroComponent = () => {
@@ -209,25 +214,215 @@ const HeroComponent = () => {
     </BlogPreview>
   );
 
+  const StyledAboutSection = createStyledMotionComponent('section')(props => `
+    color: #e6e6e6; // Soft white for readability
+    padding-bottom: 20px;
+    padding-left: 20px;
+    padding-right: 20px;
+    margin-top: 0rem;
+    padding-top: 0rem;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    max-width: 800px; // Ensuring the text is not too stretched on larger screens
+  `);
+  const StyledHeading = createStyledMotionComponent('h2')(props => `
+    color: ${projectsData.CodebasedUtils.colors.secondary};
+    margin-bottom: 12px;
+    height: 100%;
+    margin-top: 0rem;
+  `);
+  const StyledParagraph = createStyledMotionComponent('p')(props => `
+  color: #a8b2d1; // Lighter text for a modern, sleek look
+  font-size: 18px;
+  line-height: 1.7;
+  `);
+
   const AboutSectionComponent = () => (
-    <AboutSection>
-      <h2>About Me</h2>
-      <p>I am a full-stack developer with expertise in modern web technologies...</p>
-      {/* Add more personal or professional details here */}
-    </AboutSection>
+    <StyledAboutSection
+      initial={{ y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <StyledHeading>
+        Alex Figueroa | <span style={{ color: 'rgba(250,250,250,1)' }}>Full Stack Developer and Automation Expert</span>
+      </StyledHeading>
+      <StyledParagraph>
+        I’m a full-stack developer with a knack for creating Python-based automation tools that streamline and enhance developer workflows. My portfolio spans front-end innovation to back-end efficiency, with a special focus on crafting tools that simplify and automate tasks, leading to more productive and error-free development processes. I’m passionate about driving efficiency and quality in software development through intelligent automation.
+      </StyledParagraph>
+      <CTASection>
+        <CTAButton as="a" href="/projects" >
+          View My Projects
+        </CTAButton>
+        <GhostButton as="a" href="mailto:your.email@example.com">
+          Get In Touch
+        </GhostButton>
+      </CTASection>
+    </StyledAboutSection>
   );
 
-  const TechAndToolsSectionComponent = () => (
-    <TechAndToolsSection>
-      <h2>Technologies & Tools</h2>
-      <ul>
-        <li>React</li>
-        <li>Node.js</li>
-        <li>GraphQL</li>
-        {/* List more technologies and tools here */}
-      </ul>
-    </TechAndToolsSection>
+  const TechAndToolsContainer = createStyledMotionComponent('div')(props => css`
+  flex-basis: 20%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 1rem;  // Adjust this padding as needed
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`);
+
+  const Tooltip = styled.span`
+  visibility: hidden;
+  width: 120px;
+  background-color: ${props => props.theme.colors.secondary};
+  color: white;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  position: absolute;
+  z-index: 1;
+  bottom: 100%;
+  left: 50%;
+  margin-left: -60px;
+
+  /* Tooltip arrow */
+  &::after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: ${props => props.theme.colors.secondary} transparent transparent transparent;
+  }
+`;
+
+  const IconsGrid = createStyledMotionComponent('div')(props => css`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr); // 3-column grid, adjust as needed
+  gap: 1rem;
+  justify-items: space-between;
+  border: solid 0.1rem rgba(250,250,250,0.4);
+  padding: 1rem;
+`);
+
+  const IconWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+
+  &:hover ${Tooltip} {
+    visibility: visible;
+  }
+`;
+
+  const PreviewSection = createStyledMotionComponent('section')(props => css`
+  flex-basis: ${props.width ?? '100%'};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 1rem;  // Adjust this padding as needed
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`);
+
+  const PreviewContainer = createStyledMotionComponent('div')(props => css`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr); // 3-column grid, adjust as needed
+    justify-items: space-between;
+    border: solid 0.1rem rgba(250,250,250,0.4);
+    padding: 0.5rem;
+    width: 100%;
+`);
+
+  const ToolIcon = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #fff;
+  margin: 10px;
+
+  &:hover {
+    color: ${projectsData.CodebasedUtils.colors.secondary}; // Neon hover effect
+  }
+
+  svg {
+    fill: currentColor;
+    transition: transform 0.2s ease-in-out;
+  }
+
+  &:hover svg {
+    transform: scale(1.1);
+  }
+`;
+
+  const SeeAllButton = createStyledMotionComponent(motion.button)(props => css`
+  ${fontFamily()}
+  ${fontSize('xsmall')}
+  background-color: transparent;
+  color: rgba(250,250,250,0.4);
+  margin: auto;
+  margin-right: 0rem;
+  cursor: pointer;
+  text-decoration: none;
+  transition: background-color 0.3s ease, color 0.3s ease;
+
+  &:hover {
+    background-color: rgba(255, 0, 153, 0.1);
+    color: ${projectsData.CodebasedUtils.colors.secondary}; // Neon text color
+  }
+`);
+
+  const PreviewSectionTitle = createStyledMotionComponent('h2')(props => css`
+    ${fontFamily('Nova Square')}
+      `)
+
+  // PreviewSectionComponent
+  const PreviewSectionComponent = ({ title, items, buttonLabel, buttonLink, layout, flex }) => (
+    <PreviewSection flex={flex} layout={layout}>
+      <PreviewSectionTitle>{title}</PreviewSectionTitle>
+      <PreviewContainer>
+        {items.map((item, index) => (
+          <ToolIcon key={index}>
+            {item.icon}
+          </ToolIcon>
+        ))}
+      </PreviewContainer>
+      <SeeAllButton as="a" href={buttonLink}>
+        {buttonLabel}
+      </SeeAllButton>
+    </PreviewSection>
   );
+  // Component for individual icons with tooltips
+
+  // TechAndToolsSectionComponent with just a few Tools
+  function TechAndToolsSectionComponent({ flex }) {
+    return (
+      <TechAndToolsSection>
+        <PreviewSectionComponent
+          title="Favorite Tools"
+          items={[
+            { icon: <FaPython size="2em" />, label: "Python" },
+            { icon: <FaReact size="2em" />, label: "React" },
+            { icon: <FaNodeJs size="2em" />, label: "Node.js" },
+            { icon: <FaDatabase size="2em" />, label: 'PostgreSQL' },
+            { icon: <FaGitAlt size="2em" />, label: 'Git' },
+            { icon: <FaDocker size="2em" />, label: 'Docker' }
+          ]}
+          buttonLabel={"See All Tools"}
+          buttonLink="/profile"
+          layout="row"
+          flex={flex} />
+      </TechAndToolsSection >
+    );
+  }
 
 
 
@@ -264,21 +459,17 @@ const HeroComponent = () => {
           </HeroTitle>
 
         </HeroSection>
-        <CTASection>
-          <CTAButton as="a" href="/projects" >
-            View My Projects
-          </CTAButton>
-          <GhostButton as="a" href="mailto:your.email@example.com">
-            Get In Touch
-          </GhostButton>
-        </CTASection>
+
       </Top>
       <Bottom>
         <HeroContainer>
           <li style={{ backgroundImage: `url('/background.png')`, width: '60rem', height: '20rem' }}>
 
           </li>
-          <TechAndToolsSectionComponent />
+          <div style={{ display: 'flex', flexDirection: 'row', width: '100%' }}>
+            <TechAndToolsSectionComponent flex="3" />
+            <TechAndToolsSectionComponent flex="7" />
+          </div>
 
         </HeroContainer>
         <RightPane>
